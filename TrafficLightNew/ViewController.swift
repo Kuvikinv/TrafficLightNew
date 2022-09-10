a1c89bde9d8b3b21eca9e8b587b99d91dc2fc895
Kuvikinv <kuvikinvova@gmail.com>
2022-09-11 00:51:22 +0700

    Creating Light and Button

--- TrafficLightNew/ViewController.swift
+++ TrafficLightNew/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redTrafficLight: UIView!
+    @IBOutlet var yellowTrafficLight: UIView!
+    @IBOutlet var greenTrafficLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redTrafficLight.backgroundColor = UIColor(red: 1,
+                                                  green: 0,
+                                                  blue: 0,
+                                                  alpha: 0.4
+        )
+        yellowTrafficLight.backgroundColor = UIColor(red: 1,
+                                                     green: 1,
+                                                     blue: 0,
+                                                     alpha: 0.4
+        )
+        greenTrafficLight.backgroundColor = UIColor(red: 0,
+                                                    green: 1,
+                                                    blue: 0,
+                                                    alpha: 0.4
+        )
+        
+        redTrafficLight.layer.cornerRadius = 85
+        yellowTrafficLight.layer.cornerRadius = 85
+        greenTrafficLight.layer.cornerRadius = 85
+        startButton.layer.cornerRadius = 10
+        startButton.isHidden = false
+        
     }
 
-
+    @IBAction func ChangeColorButton() {
+        if startButton.isHidden{
+            startButton.setTitle("Start", for: .normal)
+        } else {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+    }
+    
 }
 
