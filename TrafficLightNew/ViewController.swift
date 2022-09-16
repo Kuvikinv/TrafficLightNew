963b1c8b7c786ede0cd3d1260f99dd316eaf7c93
Kuvikinv <kuvikinvova@gmail.com>
2022-09-16 20:21:01 +0700

    Complete

--- TrafficLightNew/ViewController.swift
+++ TrafficLightNew/ViewController.swift
@@ -12,41 +12,62 @@ class ViewController: UIViewController {
     @IBOutlet var redTrafficLight: UIView!
     @IBOutlet var yellowTrafficLight: UIView!
     @IBOutlet var greenTrafficLight: UIView!
+    
     @IBOutlet var startButton: UIButton!
+    
+    private let trafficLightOn: CGFloat = 1
+    private let trafficLightOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redTrafficLight.backgroundColor = UIColor(red: 1,
-                                                  green: 0,
-                                                  blue: 0,
-                                                  alpha: 0.4
-        )
-        yellowTrafficLight.backgroundColor = UIColor(red: 1,
-                                                     green: 1,
-                                                     blue: 0,
-                                                     alpha: 0.4
-        )
-        greenTrafficLight.backgroundColor = UIColor(red: 0,
-                                                    green: 1,
-                                                    blue: 0,
-                                                    alpha: 0.4
-        )
+//        redTrafficLight.backgroundColor = UIColor(red: 1,
+//                                                  green: 0,
+//                                                  blue: 0,
+//                                                  alpha: 0.4
+//        )
+//        yellowTrafficLight.backgroundColor = UIColor(red: 1,
+//                                                     green: 1,
+//                                                     blue: 0,
+//                                                     alpha: 0.4
+//        )
+//        greenTrafficLight.backgroundColor = UIColor(red: 0,
+//                                                    green: 1,
+//                                                    blue: 0,
+//                                                    alpha: 0.4
+//        )
+        redTrafficLight.alpha = trafficLightOff
+        yellowTrafficLight.alpha = trafficLightOff
+        greenTrafficLight.alpha = trafficLightOff
         
         redTrafficLight.layer.cornerRadius = 85
         yellowTrafficLight.layer.cornerRadius = 85
         greenTrafficLight.layer.cornerRadius = 85
+        
         startButton.layer.cornerRadius = 10
         startButton.isHidden = false
         
     }
 
+//    override func viewWillLayoutSubviews() {
+//        <#code#>
+//    }
     @IBAction func ChangeColorButton() {
-        if startButton.isHidden{
-            startButton.setTitle("Start", for: .normal)
-        } else {
+        if startButton.currentTitle == "Start" {
             startButton.setTitle("Next", for: .normal)
         }
         
+        if redTrafficLight.alpha == 1 {
+            redTrafficLight.alpha = trafficLightOff
+            yellowTrafficLight.alpha = trafficLightOn
+        } else if yellowTrafficLight.alpha == 1 {
+            yellowTrafficLight.alpha = trafficLightOff
+            greenTrafficLight.alpha = trafficLightOn
+        } else if greenTrafficLight.alpha == 1 {
+            greenTrafficLight.alpha = trafficLightOff
+            redTrafficLight.alpha = trafficLightOn
+        } else {
+            redTrafficLight.alpha = trafficLightOn
+        }
     }
     
 }
